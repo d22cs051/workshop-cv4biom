2d9bb79025cc55fb19a9ff083de889035004a181
d22cs051 <d22cs051@iitj.ac.in>
2025-10-19 01:29:39 +0530

    updated sch, pic & bio.

--- assets/js/config.js
+++ assets/js/config.js
@@ -13,7 +13,7 @@ const workshop_date = `October 19-20, 2025`
 //   'cj': ['Chad Jenkins', './assets/images/talks/chad_jenkins.jpg', 'Professor', 'University of Michigan', 'https://robotics.umich.edu/profile/chad-jenkins', 'Nonparametric Belief Propagation: Differentiable, Diagnosable, and Decisive', ''],
 // }
 const talk_speaker_details = {
-  'ggm': ['Gerard Guy Medioni', 'assets/images/talks/Gerard-Medioni.jpg', 'Professor', 'USC Viterbi School of Engineering', 'https://viterbi.usc.edu/directory/faculty/Medioni/Gerard'],
+  'ggm': ['Gerard Guy Medioni', 'assets/images/talks/Gerard-Medioni.jpg', 'Vice President and Distinguished Scientist', 'Prime Video & Studios', 'https://viterbi.usc.edu/directory/faculty/Medioni/Gerard'],
   'lpm': ['Louis-Philippe Morency', 'assets/images/talks/louis_morency.jpg', 'Associate Professor', 'School of Computer Science, Carnegie Mellon University', 'https://www.cs.cmu.edu/~morency/'],
   'laj': ['László A. Jeni', 'assets/images/talks/Laszlo_AJ.jpg', 'Assistant Professor', 'Robotics Institute of Carnegie Mellon University', 'https://www.laszlojeni.com/'],
   'ak': ['Ajay Kumar', 'assets/images/keynotes/ajay_kumar.png', 'Associate Head (Research) and Professor', 'Hong Kong Polytechnic University', 'https://www.polyu.edu.hk/dsai/people/academic-staff/kumarajay/?sc_lang=en'],
